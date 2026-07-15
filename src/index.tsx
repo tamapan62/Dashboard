dd7d5aca38f8c7efe0ea2583827d12b63c672e76
Apisit Tamapan <tamapan62@gmail.com>
2026-07-15 07:54:35 +0000

    14.54

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// src/index.tsx
+window.location.replace("/dashboard.html?v=20260715-no-app-import");
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+export {};
