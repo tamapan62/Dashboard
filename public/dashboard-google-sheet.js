e2ef2e2cda3132f1a483c0a499976c828aedec6a
tamapan62 <tamapan62@gmail.com>
2026-07-19 11:21:34 +0000

    19.7.69

--- public/dashboard-google-sheet.js
+++ public/dashboard-google-sheet.js
@@ -1,8 +1,8 @@
 (function () {
   const DEFAULT_CONFIG = {
     enabled: true,
-    storesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQz51QMZELZgPyqIWRGUJFqq7b0NJwpQq4rNBCPnKbEk8KIq8Lx8hTwOTvDsLkigqrLUPrbH81wD8Dm/pub?output=csv",
-    callsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSCl9wpZxP3GeGyP-b3865kPRVdI--4auVNA9IshAg7NvpVhvlXLG27GnYZVDwqtb-hgIEwJ5SrTPVY/pub?output=csv",
+    storesCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuiyu9L_E4nUxC6ixeEjD3YCE7_DxyB0cFd9nAxIEFRsF7RVynDQhUbsMmjhuguA/pub?output=csv",
+    callsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQz51QMZELZgPyqIWRGUJFqq7b0NJwpQq4rNBCPnKbEk8KIq8Lx8hTwOTvDsLkigqrLUPrbH81wD8Dm/pub?output=csv",
   };
   const userConfig = window.GOOGLE_SHEETS_CONFIG || {};
   const config = {
